f5d15cf915268f3e0db94beba05b05662a3556e9
ArezooZafardoost <arezoo.zafardoost@gmail.com>
2023-10-23 20:49:34 +0330

    javascript

---
+++ script2.js
@@ -0,0 +1,31 @@
+function classifier(array) {
+    let num = 0, str = 0, obj = 0, arr = 0, bool = 0, undef = 0, nul = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            arr++;
+        } else if (typeof array[i] === 'object' && array[i] === null) {
+            nul++
+        } else {
+            switch (typeof array[i]) {
+                case 'number':
+                    num++;
+                    break;
+                case 'string':
+                    str++;
+                    break;
+                case 'object':
+                    obj++;
+                    break;
+                case 'undefined':
+                    undef++;
+                    break;
+                case 'boolean':
+                    bool++;
+                    break;
+            }
+        }
+    }
+    console.log(`number : ${num} , string : ${str} , object : ${obj} , array : ${arr} , boolean : ${bool} , undefined : ${undef} , null : ${nul}`);
+}
+
+classifier([2, 5, 'amin', { a: 1 }, [1, 2], true, undefined, null, [3, 4], { b: 3 }]);
